df52f3129295bee9133ebefb411f06e440d82991
Chris Kent <8364109+thechriskent@users.noreply.github.com>
2025-03-31 13:43:47 -0400

    Initial Commit

---
+++ src/webparts/shellfies/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IShellfiesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ShellfiesWebPartStrings' {
+  const strings: IShellfiesWebPartStrings;
+  export = strings;
+}
